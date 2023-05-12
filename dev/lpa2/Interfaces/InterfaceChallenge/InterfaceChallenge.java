fed167492e69c75cf15e3af6d97b28268a8d5954
Annoyingberry <isaacyqloh@gmail.com>
2023-05-12 17:42:39 +0800

    #

---
+++ dev/lpa2/Interfaces/InterfaceChallenge/InterfaceChallenge.java
@@ -0,0 +1,7 @@
+package Javapractice.dev.lpa2.Interfaces.InterfaceChallenge;
+
+public class InterfaceChallenge {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ dev/lpa3/GenericsMain.java
@@ -0,0 +1,5 @@
+package Javapractice.dev.lpa3;
+
+public class GenericsMain {
+	
+}
